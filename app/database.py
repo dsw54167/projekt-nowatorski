5362995343a1ed54a63d3f1eb5cd7fb475c111ff
Michał Młynarczyk <michal.mlynarczyk@benefitsystems.pl>
2025-12-28 14:49:34 +0100

    lint

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASS = os.getenv("POSTGRES_PASSWORD")
