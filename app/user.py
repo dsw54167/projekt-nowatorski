a9b26956e23900106c081ea9974689a532a52b84
Michał Młynarczyk <michal.mlynarczyk@benefitsystems.pl>
2025-12-28 15:06:09 +0100

    lint fix

--- app/user.py
+++ app/user.py
@@ -1,7 +1,13 @@
+"""User model definition."""
 from sqlalchemy import Column, Integer, String
 from .database import Base
 
 class User(Base):
+    """User database model.
+
+   Represents a user in the database with basic information
+   like ID and email address.
+   """
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
